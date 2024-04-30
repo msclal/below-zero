d22b54e054cf79b6852acf9fa657dad242dadce9
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2024-04-30 00:50:31 -0700

    rewards on landing done -- responsive and all

--- src/components/Landing/Rewards.jsx
+++ src/components/Landing/Rewards.jsx
@@ -1,29 +1,45 @@
 import React from "react";
 import Image from "next/image";
-import RewardCards from "/public/Rewards.png";
+import MobileCards from "/public/MobileRewards.webp";
+import Button from "../Button";
 
 const Rewards = () => {
   return (
-    <div className="pt-36 flex">
-      <div>
-        <div className="font-semibold text-3xl">Rewards Card</div>
-        <div className="font-light text-text-subtext">
-          Ask for a Rewards Card! Just for being a part of our shaved ice
-          family, you&#39;ll earn points with every purchase. Accumulate enough
-          points and watch as your favorite frosty treat becomes absolutely
-          FREE. Find our location now!
+    <div className="flex py-[5%] pt-36 justify-center">
+      <div className="flex max-sm:flex-col w-10/12 justify-center max-sm:items-center space-x-[8%]">
+        <div className="sm:hidden w-[100%] pb-[10%]">
+          <Image
+            src={MobileCards}
+            alt="Landing"
+            layout="responsive"
+            className=""
+            width="1"
+            height="1"
+          />
+        </div>{" "}
+        <div className="flex flex-col max-sm:w-full max-sm:items-center justify-center space-y-5 w-7/12">
+          <div className="font-semibold text-4xl">Rewards Card</div>
+          <div className="font-light text-text-subtext max-sm:text-center">
+            Ask for a Rewards Card! Just for being a part of our shaved ice
+            family, you&#39;ll earn points with every purchase. Accumulate
+            enough points and watch as your favorite frosty treat becomes
+            absolutely FREE. Find our location now!
+          </div>
+          <div className="w-fit">
+            <Button>OUR LOCATION</Button>
+          </div>
         </div>
+        <div className="max-sm:hidden w-3/4 ">
+          <Image
+            src={MobileCards}
+            alt="Landing"
+            layout="responsive"
+            className=""
+            width="1"
+            height="1"
+          />
+        </div>{" "}
       </div>
-      <div className="w-4/12">
-        <Image
-          src={RewardCards}
-          alt="Landing"
-          layout="responsive"
-          className=""
-          width="1"
-          height="1"
-        />
-      </div>{" "}
     </div>
   );
 };
