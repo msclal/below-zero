a3b640a5b0512a0286c047a7ce6f8a4d7de9a7ce
msclal <mika1shanela@gmail.com>
2024-05-01 00:56:24 -0700

    add states - WIP

--- src/components/Menu/Menubar.jsx
+++ src/components/Menu/Menubar.jsx
@@ -1,40 +1,36 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 
-const Menubar = () => {
-  const [isClicked, setIsClicked] = useState("SHAVED ICE");
-  const handleClick = (item) => {
-    setIsClicked(item);
-  };
+const Menubar = ({ selectedMenuItem, onMenuItemClick }) => {
   return (
     <div className="relative flex flex-col w-1/6 font-medium gap-y-3.5">
       <p
-        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-blue-200/90 hover:text-white   ${isClicked === "SHAVED ICE" ? "bg-below-zero-blue-200 text-white" : "text-black bg-white border"}`}
-        onClick={() => handleClick("SHAVED ICE")}
+        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-blue-200/90 hover:text-white   ${selectedMenuItem === "SHAVED ICE" ? "bg-below-zero-blue-200 text-white" : "text-black bg-white border"}`}
+        onClick={() => onMenuItemClick("SHAVED ICE")}
       >
         SHAVED ICE
       </p>
       <p
-        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-red-200/90 hover:text-white  ${isClicked === "SHAVED ICE COMBOS" ? "bg-below-zero-red-200 text-white" : "text-black bg-white border border-border"}`}
-        onClick={() => handleClick("SHAVED ICE COMBOS")}
+        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-red-200/90 hover:text-white  ${selectedMenuItem === "SHAVED ICE COMBOS" ? "bg-below-zero-red-200 text-white" : "text-black bg-white border border-border"}`}
+        onClick={() => onMenuItemClick("SHAVED ICE COMBOS")}
       >
         SHAVED ICE COMBOS
       </p>
       <p
-        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-yellow-200/90 hover:text-white  ${isClicked === "ICE CREAM" ? "bg-below-zero-yellow-200 text-white" : "text-black bg-white border border-border"}`}
-        onClick={() => handleClick("ICE CREAM")}
+        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-yellow-200/90 hover:text-white  ${selectedMenuItem === "ICE CREAM" ? "bg-below-zero-yellow-200 text-white" : "text-black bg-white border border-border"}`}
+        onClick={() => onMenuItemClick("ICE CREAM")}
       >
         ICE CREAM
       </p>
       <p
-        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out  hover:bg-below-zero-purple-200/90 hover:text-white  ${isClicked === "SHAKES" ? "bg-below-zero-purple-200 text-white" : "text-black bg-white border border-border"}`}
-        onClick={() => handleClick("SHAKES")}
+        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out  hover:bg-below-zero-purple-200/90 hover:text-white  ${selectedMenuItem === "SHAKES" ? "bg-below-zero-purple-200 text-white" : "text-black bg-white border border-border"}`}
+        onClick={() => onMenuItemClick("SHAKES")}
       >
         SHAKES
       </p>
       <p
-        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-green-200/90 hover:text-white  ${isClicked === "HOUSE SPECIALTIES" ? "bg-below-zero-green-200 text-white" : "text-black bg-white border border-border"}`}
-        onClick={() => handleClick("HOUSE SPECIALTIES")}
+        className={`p-4 rounded-r-lg cursor-pointer transition-all duration-300 ease-in-out hover:bg-below-zero-green-200/90 hover:text-white  ${selectedMenuItem === "HOUSE SPECIALTIES" ? "bg-below-zero-green-200 text-white" : "text-black bg-white border border-border"}`}
+        onClick={() => onMenuItemClick("HOUSE SPECIALTIES")}
       >
         HOUSE SPECIALTIES
       </p>
