d63c6b1ac64d2d034f4d15630b3472668cb521c8
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2024-05-05 15:28:02 -0700

    banner and video

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,25 @@ import Rewards from "@/components/Landing/Rewards";
 import Services from "@/components/Landing/Services";
 import Video from "@/components/Landing/Video";
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
+import Marquee from "react-fast-marquee";
+import IceCreamBanner from "/public/IceCreamBanner.webp";
 
 const page = () => {
   return (
     <>
+      <Marquee autoFill speed={30}>
+        <div className="">
+          <Image
+            src={IceCreamBanner}
+            alt="Landing"
+            layout="responsive"
+            className=""
+          />
+        </div>
+      </Marquee>
+
       <Head>
         <title>Below Zero: Shaved Ice</title>
       </Head>
