06daf33241392611bfa0bd73f3c85969f9550a06
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2024-05-31 11:51:52 -0700

    gallery mods

--- src/components/About/Gallery.js
+++ src/components/About/Gallery.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import Sample from "/public/about/sample.png";
-import Link from "next/link";
+import Image1 from "/public/about/Gallery1.webp";
+import Image2 from "/public/about/Gallery2.webp";
+import Image3 from "/public/about/Gallery3.webp";
+import Image4 from "/public/about/Gallery4.webp";
+
+// import Link from "next/link";
 const Gallery = () => {
   return (
     <div className="flex flex-col px-5 sm:items-center sm:justify-center gap-y-10">
@@ -13,119 +17,15 @@ const Gallery = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-3 gap-1 sm:w-3/4 sm:grid-cols-4 lg:grid-cols-4">
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative w-full bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative w-full bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative w-full bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative w-full bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative w-full bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative w-full bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative hidden w-full sm:block bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
-        <Link
-          href="https://www.instagram.com/belowzeroshavedice/?hl=en"
-          target="_blank"
-          className="relative hidden w-full sm:block bg-secondary-background aspect-square"
-        >
-          <Image
-            src={Sample}
-            alt="shaved ice"
-            priority={true}
-            fill
-            draggable={false}
-            className="object-cover"
-          />
-        </Link>
+      {/* <div className="grid grid-cols-2 gap-1 sm:w-3/4 sm:grid-cols-2 lg:grid-cols-2"> */}
+
+      <div className="max-sm:w-full max-sm:pb-[25%] sm:w-11/12 lg:w-10/12 xl:w-8/12 2xl:w-7/12">
+        <div className="grid grid-cols-2 gap-1">
+          <Image src={Image1} alt="shaved ice" draggable={false} className="" />
+          <Image src={Image2} alt="shaved ice" draggable={false} className="" />
+          <Image src={Image3} alt="shaved ice" draggable={false} className="" />
+          <Image src={Image4} alt="shaved ice" draggable={false} className="" />
+        </div>
       </div>
     </div>
   );
