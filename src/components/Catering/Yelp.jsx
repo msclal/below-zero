70bd2d82c62875873fcc2b39cd81d63d03c5f98b
msclal <mika1shanela@gmail.com>
2024-06-02 00:16:23 -0700

    add more yelp reviews

--- src/components/Catering/Yelp.jsx
+++ src/components/Catering/Yelp.jsx
@@ -11,42 +11,36 @@ const Yelp = () => {
         See what our customers are saying!
       </p>
 
+      {/* Tablet */}
       <div className="relative grid grid-cols-1 gap-y-5 sm:hidden">
         <span
-          className="yelp-review"
-          data-review-id="fz0OCixkLNFdUOiv-bx2vA"
+          class="yelp-review"
+          data-review-id="ZI6Q78zR6y3VKnqYVkuHgg"
           data-hostname="www.yelp.com"
         >
           Read{" "}
           <a
-            target="_blank"
-            href="https://www.yelp.com/user_details?userid=ftF0UAOs9LmmbVfKm7UCgA"
+            href="https://www.yelp.com/user_details?userid=q0jeGUh60S-D3lw2xoBl_w"
             rel="nofollow noopener"
           >
-            Danny P.
+            Lance U.
           </a>
           {"'s "}
           <a
-            target="_blank"
-            href="https://www.yelp.com/biz/below-zero-shaved-ice-upland?hrid=fz0OCixkLNFdUOiv-bx2vA"
+            href="https://www.yelp.com/biz/below-zero-shaved-ice-upland?hrid=ZI6Q78zR6y3VKnqYVkuHgg"
             rel="nofollow noopener"
           >
             review
           </a>{" "}
           of{" "}
           <a
-            target="_blank"
             href="https://www.yelp.com/biz/pHYLIh3wLUriqiUg8DrvcA"
             rel="nofollow noopener"
           >
             Below Zero Shaved Ice
           </a>{" "}
           on{" "}
-          <a
-            target="_blank"
-            href="https://www.yelp.com"
-            rel="nofollow noopener"
-          >
+          <a href="https://www.yelp.com" rel="nofollow noopener">
             Yelp
           </a>
           <script
@@ -133,40 +127,33 @@ const Yelp = () => {
           />
         </div>
         <span
-          className="yelp-review"
-          data-review-id="fz0OCixkLNFdUOiv-bx2vA"
+          class="yelp-review"
+          data-review-id="ZI6Q78zR6y3VKnqYVkuHgg"
           data-hostname="www.yelp.com"
         >
           Read{" "}
           <a
-            target="_blank"
-            href="https://www.yelp.com/user_details?userid=ftF0UAOs9LmmbVfKm7UCgA"
+            href="https://www.yelp.com/user_details?userid=q0jeGUh60S-D3lw2xoBl_w"
             rel="nofollow noopener"
           >
-            Danny P.
+            Lance U.
           </a>
           {"'s "}
           <a
-            target="_blank"
-            href="https://www.yelp.com/biz/below-zero-shaved-ice-upland?hrid=fz0OCixkLNFdUOiv-bx2vA"
+            href="https://www.yelp.com/biz/below-zero-shaved-ice-upland?hrid=ZI6Q78zR6y3VKnqYVkuHgg"
             rel="nofollow noopener"
           >
             review
           </a>{" "}
           of{" "}
           <a
-            target="_blank"
             href="https://www.yelp.com/biz/pHYLIh3wLUriqiUg8DrvcA"
             rel="nofollow noopener"
           >
             Below Zero Shaved Ice
           </a>{" "}
           on{" "}
-          <a
-            target="_blank"
-            href="https://www.yelp.com"
-            rel="nofollow noopener"
-          >
+          <a href="https://www.yelp.com" rel="nofollow noopener">
             Yelp
           </a>
           <script
@@ -239,40 +226,33 @@ const Yelp = () => {
           />
         </div>
         <span
-          className="yelp-review"
-          data-review-id="fz0OCixkLNFdUOiv-bx2vA"
+          class="yelp-review"
+          data-review-id="a-6bHpurDQY8sl0bqQrSFw"
           data-hostname="www.yelp.com"
         >
           Read{" "}
           <a
-            target="_blank"
-            href="https://www.yelp.com/user_details?userid=ftF0UAOs9LmmbVfKm7UCgA"
+            href="https://www.yelp.com/user_details?userid=Jz-LCdZl-Wh-UH0niHadKQ"
             rel="nofollow noopener"
           >
-            Danny P.
+            Super G.
           </a>
           {"'s "}
           <a
-            target="_blank"
-            href="https://www.yelp.com/biz/below-zero-shaved-ice-upland?hrid=fz0OCixkLNFdUOiv-bx2vA"
+            href="https://www.yelp.com/biz/below-zero-shaved-ice-upland?hrid=a-6bHpurDQY8sl0bqQrSFw"
             rel="nofollow noopener"
           >
             review
           </a>{" "}
           of{" "}
           <a
-            target="_blank"
             href="https://www.yelp.com/biz/pHYLIh3wLUriqiUg8DrvcA"
             rel="nofollow noopener"
           >
             Below Zero Shaved Ice
           </a>{" "}
           on{" "}
-          <a
-            target="_blank"
-            href="https://www.yelp.com"
-            rel="nofollow noopener"
-          >
+          <a href="https://www.yelp.com" rel="nofollow noopener">
             Yelp
           </a>
           <script
