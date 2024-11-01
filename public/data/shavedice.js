4ed2bc9109ebb7bc54813ef707a947ecb690c123
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2024-11-01 11:51:49 -0700

    added visible field to menu item

--- public/data/shavedice.js
+++ public/data/shavedice.js
@@ -4,56 +4,66 @@ export const shavedice = [
       "/product_pictures/shavedice/BananaStrawberryandCakeBatter/BananaStrawberryandCakeBatter.webp",
     name: "Banana Strawberry & Cake Batter",
     description: "",
+    visible: true,
   },
   {
     picture:
       "/product_pictures/shavedice/BlueGumAndCherry/BlueGumAndCherry.webp",
     name: "Blue Gum & Cherry",
     description: "",
+    visible: true,
   },
   {
     picture:
       "/product_pictures/shavedice/BlueRaspberryStrawberryAndPinaColada/BlueRaspberryStrawberryAndPinaColada.webp",
     name: "Blue Raspberry, Strawberry, & Pina Colada",
     description: "",
+    visible: true,
   },
   {
     picture:
       "/product_pictures/shavedice/CaramelAndPraline/CaramelAndPraline.webp",
     name: "Caramel & Praline",
     description: "",
+    visible: true,
   },
   {
     picture: "/product_pictures/shavedice/CherryAndCola/CherryAndCola.webp",
     name: "Cherry & Cola",
     description: "",
+    visible: true,
   },
   {
     picture:
       "/product_pictures/shavedice/CherryLimeAndPineapple/CherryLimeAndPineapple.webp",
     name: "Cherry Lime & Pineapple",
     description: "",
+    visible: true,
   },
   {
     picture: "/product_pictures/shavedice/LimeAndCherry/LimeAndCherry.webp",
     name: "Lime & Cherry",
     description: "",
+    visible: true,
   },
   {
     picture:
       "/product_pictures/shavedice/SourAppleOrangeAndPineapple/SourAppleOrangeAndPineapple.webp",
     name: "Sour Apple, Orange, & Pineapple",
     description: "",
+    visible: true,
   },
   {
     picture:
       "/product_pictures/shavedice/SourAppleTamarindAndOrange/SourAppleTamarindAndOrange.webp",
     name: "Sour Apple, Tamarind, & Orange",
     description: "",
+    visible: true,
   },
   {
     picture: "/product_pictures/shavedice/Unicorn/Unicorn.webp",
     name: "Unicorn",
     description: "",
+    visible: true,
   },
 ];
