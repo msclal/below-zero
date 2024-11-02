88b3209f94e990417446c27365df27364fa4ad29
Bobby Lerias <72523111+bobbyyy57@users.noreply.github.com>
2024-11-02 11:48:50 -0700

    Update shavedice.js

--- public/data/shavedice.js
+++ public/data/shavedice.js
@@ -4,7 +4,7 @@ export const shavedice = [
       "/product_pictures/shavedice/BananaStrawberryandCakeBatter/BananaStrawberryandCakeBatter.webp",
     name: "Banana Strawberry & Cake Batter",
     description: "",
-    visible: false,
+    visible: true,
   },
   {
     picture:
